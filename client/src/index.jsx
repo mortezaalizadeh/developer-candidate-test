2c5d37b28ee6764005984d45c9ad3cc564a2ddd9
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-05-12 14:50:40 +1200

    Fixed linting issue

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,18 @@
 import 'regenerator-runtime/runtime';
 import 'typeface-roboto';
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { reduxStore } from './framework/redux';
 import i18n from './i18n';
 
-class AppRoot extends Component {
-  render = () => (
-    <I18nextProvider i18n={i18n}>
-      <Provider store={reduxStore}>
-        <h1>This is a test</h1>
-      </Provider>
-    </I18nextProvider>
-  );
-}
+const AppRoot = () => (
+  <I18nextProvider i18n={i18n}>
+    <Provider store={reduxStore}>
+      <h1>This is a test</h1>
+    </Provider>
+  </I18nextProvider>
+);
 
 ReactDOM.render(<AppRoot />, document.getElementById('app'));
