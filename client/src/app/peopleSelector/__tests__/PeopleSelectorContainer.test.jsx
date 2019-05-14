765a1265f817ff9d837254e9353179de66d2b7a5
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-05-15 11:05:20 +1200

    Added more unit test

--- client/src/app/peopleSelector/__tests__/PeopleSelectorContainer.test.jsx
+++ client/src/app/peopleSelector/__tests__/PeopleSelectorContainer.test.jsx
@@ -7,15 +7,24 @@ import { PersonResultContainer } from '../../personResult';
 
 const chance = new Chance();
 
+class HistoryMock {
+  constructor() {
+    this.push = jest.fn();
+  }
+}
+
 describe('PeopleSelectorContainerComponent', () => {
   let selectedFilter;
   let component;
+  let history;
 
   beforeEach(() => {
     selectedFilter = chance.string();
+    history = new HistoryMock();
 
     const injectedProps = {
       selectedFilter,
+      history,
     };
 
     component = shallow(<PeopleSelectorContainerComponent {...injectedProps} />);
@@ -32,5 +41,50 @@ describe('PeopleSelectorContainerComponent', () => {
     const foundComponent = component.find(PersonResultContainer);
 
     expect(foundComponent).toBeDefined();
+  });
+
+  it('should navigate to /everybody link when everybody button event raised', () => {
+    const foundComponent = component.find(PeopleSelector);
+
+    foundComponent.props().onSelectorSelected('everybody');
+
+    expect(history.push.mock.calls).toHaveLength(1);
+    expect(history.push.mock.calls[0][0]).toBe('/everybody');
+  });
+
+  it('should navigate to /male link when male button event raised', () => {
+    const foundComponent = component.find(PeopleSelector);
+
+    foundComponent.props().onSelectorSelected('male');
+
+    expect(history.push.mock.calls).toHaveLength(1);
+    expect(history.push.mock.calls[0][0]).toBe('/male');
+  });
+
+  it('should navigate to /female link when female button event raised', () => {
+    const foundComponent = component.find(PeopleSelector);
+
+    foundComponent.props().onSelectorSelected('female');
+
+    expect(history.push.mock.calls).toHaveLength(1);
+    expect(history.push.mock.calls[0][0]).toBe('/female');
+  });
+
+  it('should navigate to /over30 link when over30 button event raised', () => {
+    const foundComponent = component.find(PeopleSelector);
+
+    foundComponent.props().onSelectorSelected('over30');
+
+    expect(history.push.mock.calls).toHaveLength(1);
+    expect(history.push.mock.calls[0][0]).toBe('/over30');
+  });
+
+  it('should navigate to /under30 link when under30 button event raised', () => {
+    const foundComponent = component.find(PeopleSelector);
+
+    foundComponent.props().onSelectorSelected('under30');
+
+    expect(history.push.mock.calls).toHaveLength(1);
+    expect(history.push.mock.calls[0][0]).toBe('/under30');
   });
 });
